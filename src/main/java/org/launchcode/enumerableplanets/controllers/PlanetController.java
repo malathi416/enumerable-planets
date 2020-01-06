d71820f5f4bda8495e80130f7e65ce2cfa1bf80b
Malathi416 <ammu.416@gmail.com>
2020-01-06 11:25:01 -0600

    completed enums exercise

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,13 +1,20 @@
 package org.launchcode.enumerableplanets.controllers;
 
+import org.launchcode.enumerableplanets.data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+//        ArrayList<Planets> planets = new ArrayList<>();
+//        planets.add(Planets.MERCURY);
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
